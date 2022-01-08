83158a0653412cd46fd8d8cff48968f2aae7c5e4
BaleineSousCailloux <djfreez29@gmail.com>
2022-01-08 14:38:38 +0100

    lightbox open ok - close ko !

--- public/scripts/lightbox.js
+++ public/scripts/lightbox.js
@@ -1,34 +1,36 @@
 // DOM Elements
-const openLightbox = document.getElementsByClassName(".portfolio__content__card__img")
-const lightbox = document.getElementById("lightbox");
-const closeLightbox = document.getElementById("closeLightbox");
+const openLightbox = document.querySelectorAll(".portfolio__content__card__img");
+const lightbox = document.querySelector("#lightbox");
+const closeLightbox = document.querySelector("#closeBox");
 
 
-// launch Lightbox event (img click)
+
+// open Lightbox event (img click)
 openLightbox.forEach(img => img.addEventListener("click", launchLightbox));
 
 
-// launch lightbox fonction
+// open lightbox fonction
 function launchLightbox() {
-  lightbox.style.display = 'flex';
-}
+  lightbox.style.display = "flex";
+};
 
 
-// options : delay to close
+/* options : delay to close
 let delayToClose;
 function manualCloseDelay() {
   delayToClose = setTimeout(quitLightbox, 500);
 }
 function autoCloseDelay() {
   delayToClose = setTimeout(quitLightbox, 3500);
-}
-
-
-// close lightbox event
-closeLightbox.addEventListener('click', manualCloseDelay);
-
+}*/
 
 // close lightbox function
 function quitLightbox() {
-  lightbox.style.display = 'none';
-}
+  lightbox.style.display = "none";
+};
+
+
+// close lightbox event
+closeLightbox.addEventListener("click", quitLightbox);
+
+
