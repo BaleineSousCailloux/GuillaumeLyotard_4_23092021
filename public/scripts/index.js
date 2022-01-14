0fafc5d19b052d7fdc8f05b532ec2c46fb92ce45
BaleineSousCailloux <djfreez29@gmail.com>
2022-01-14 14:53:24 +0100

    récupération des tags dans le json pour insertion dans le header

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,6 @@
 let photographers = [];
 let medias = [];
-let tags = [];
+let listTags = [];
 
 const fetchData = async () => {
     const res = await fetch("./public/data/FishEyeData.json");
@@ -15,6 +15,39 @@ const getMediasData = async () => {
     const data = await fetchData();
     return data.media;
 }
+
+const getTagsListData = async () => {
+    photographers = await getPhotographersData();
+    photographers.forEach(photographer => {
+        photographer.tags.forEach(tag => {
+            if (!listTags.includes(tag)) {
+                listTags.push(tag)
+            };
+        });
+    });
+    return listTags;
+}
+
+const headerTagFactory = (headerTag) => {
+    getHeaderTag = () => {
+        const newHeaderTag = document.createElement("span");
+        newHeaderTag.classList.add("tag");
+        newHeaderTag.innerHTML = `#${headerTag}`;
+        return newHeaderTag;
+    }
+    return this;
+}
+
+const getHeaderTagsList = async () => {
+    listTags = await getTagsListData();
+    console.log(listTags) // verif
+    listTags.forEach(tag => {
+        const instance = headerTagFactory(tag);
+        const newHeaderTag = instance.getHeaderTag();
+        document.getElementById("tagsList").appendChild(newHeaderTag);
+    })
+};
+getHeaderTagsList();
 
 const indexCardFactory = (indexCard) => {
     getCard = () => {
@@ -33,9 +66,9 @@ const indexCardFactory = (indexCard) => {
         </div>
         `;
         indexCard.tags.forEach(tag => {
-            newTag = document.createElement("span");
+            const newTag = document.createElement("span");
             newTag.classList.add("photographer__legend__tags__tag");
-            newTag.innerText = tag;
+            newTag.innerHTML = `#${tag}`;
             newPhotographer.getElementsByClassName("photographer__legend__tags")[0].appendChild(newTag);
         })
         return newPhotographer;
